a94d5af64013cbbb580b614be721a9f08c67b2bf
alexrosl <reshetnikov.alex5@yandex.ru>
2020-07-09 14:11:13 +0300

    Add flask to generate reports

---
+++ app.py
@@ -0,0 +1,32 @@
+import pandas
+from flask import Flask, render_template
+
+from src.db.db_utils import DbUtil
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    pandas.set_option('display.max_colwidth', None)
+    query = open("src/all/all_resources.sql", "r").read()
+
+    db_util = DbUtil()
+    df = pandas.read_sql(sql=query, con=db_util.db)
+
+    columns = ["source",
+               "id",
+               "author",
+               "link",
+               "text",
+               "datetime",
+               "created"
+               ]
+    df['link'] = df['link'].apply(lambda x: '<a href="{0}">Ссылка</a>'.format(x))
+
+    html_text = df.to_html(columns=columns, escape=False, index=False).replace(r"\n", "<br>")
+    return render_template("index_template.html", text=html_text, order_column="5")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
